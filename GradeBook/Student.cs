60cfd9caf6b425785a335769b2259b629e60b6ea
G-Romanczuk <gxbiromanczuk@gmail.com>
2022-03-03 19:41:54 +0100

    gpa

--- GradeBook/Student.cs
+++ GradeBook/Student.cs
@@ -25,6 +25,7 @@ namespace GradeBook
         public char LetterGrade { get; set; }
         [JsonIgnore]
         public double GPA { get; set; }
+        
 
         public Student(string name, StudentType studentType, EnrollmentType enrollment)
         {
